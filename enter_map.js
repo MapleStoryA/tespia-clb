f2960a6beb5b40b802ae0dc69c4f6dccf1e14ac9
vmuser@github.com <vmuser@github.com>
2020-06-21 02:57:50 -0300

    Added example.

--- enter_map.js
+++ enter_map.js
@@ -1,2 +1,7 @@
-client.connect('127.0.0.1', 8484);
+var ip = '127.0.0.1';
+client.connect(ip, 8484);
 client.login('admin', 'admin', '1111', 0, 0);
+
+function onConnectInGame(){
+ controller.log('Character is connected in game.')
+}
